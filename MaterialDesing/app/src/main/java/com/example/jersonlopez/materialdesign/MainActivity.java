ca9d3a268fd576326e872c62c61b9b636b0a0507
jersonlopez <jersonlopez.1204@gmail.com>
2017-08-16 16:14:36 -0500

    iniciando tutorial codelabs#

---
+++ MaterialDesing/app/src/main/java/com/example/jersonlopez/materialdesign/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.jersonlopez.materialdesign;
+
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Tab 1"));
+        tabs.addTab(tabs.newTab().setText("Tab 2"));
+        tabs.addTab(tabs.newTab().setText("Tab 3"));
+
+        // Adding Toolbar to Main screen
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
