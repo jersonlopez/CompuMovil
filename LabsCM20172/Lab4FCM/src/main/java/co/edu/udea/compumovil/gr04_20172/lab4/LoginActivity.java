008219e9d97b86aa4c72bb931e5f33e312fd864f
jersonlopez <jersonlopez.1204@gmail.com>
2017-10-23 02:28:44 -0500

    signin with Google

--- LabsCM20172/Lab4FCM/src/main/java/co/edu/udea/compumovil/gr04_20172/lab4/LoginActivity.java
+++ LabsCM20172/Lab4FCM/src/main/java/co/edu/udea/compumovil/gr04_20172/lab4/LoginActivity.java
@@ -12,21 +12,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    Button Starting, Register;
-    EditText email, password;
-    //DbHelper dbHelper;
-    //SQLiteDatabase db;
+    private SignInButton btnGoogle;
+    private Button Starting, Register;
+    private EditText email, password;
+    private static final int RC_SIGN_IN = 1;
+    private GoogleApiClient mGoogleApiClient;
 
 
     @Override
@@ -34,14 +44,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        btnGoogle = (SignInButton) findViewById(R.id.signInButton);
         Starting = (Button)findViewById(R.id.buttonLogin);
         Register = (Button)findViewById(R.id.buttonLogUp);
         email = (EditText) findViewById(R.id.editTextEmailLogin);
         password = (EditText) findViewById(R.id.editTextPasswordLogin);
         Starting.setOnClickListener(this);
         Register.setOnClickListener(this);
-        //dbHelper = new DbHelper(this);
-        //db= dbHelper.getWritableDatabase();
+        btnGoogle.setOnClickListener(this);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -60,8 +70,83 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             }
         };
 
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                } /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+        singInGoogle();
+    }
+
+    public void singInGoogle(){
+        Toast.makeText(getApplicationContext(), "Login exitoso", Toast.LENGTH_SHORT).show();
+        Intent intentNavigation = new Intent(LoginActivity.this, Navigation_Drawer.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        //intentNavigation.putExtra("email", user);
+        //Toast.makeText(getApplicationContext(), user, Toast.LENGTH_SHORT).show();
+        startActivity(intentNavigation);
+        finish();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
 
     @Override
     public void onStart() {
@@ -97,14 +182,16 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                     return;
                 }
                 Login(id_Customer, pass_Customer);
-
-
                 break;
 
             case R.id.buttonLogUp:
                 //Log.d("TAG","pase por aca");
                 Intent intent1 = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent1);
+
+            case  R.id.signInButton:
+                signIn();
+                break;
         }
     }
 
